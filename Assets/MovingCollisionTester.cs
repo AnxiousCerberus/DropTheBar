a813c44a592c2aad4849e97855bd32749b178f9a
BLUE-BLUR\Lerenwe <johan.nagy.contact@gmail.com>
2017-12-11 00:13:37 +0100

    Experimenting... Stuff....

---
+++ Assets/MovingCollisionTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCollisionTester : MonoBehaviour {
+
+    public float Speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+	}
+}
